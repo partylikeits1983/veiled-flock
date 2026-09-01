bb1cf1145ab97dfa6d671680cce423e88ca406b0
Andrei Ivasko <cyberbono3@gmail.com>
2026-09-01 15:23:26 +0300

    chore: remove non-doc changes from PR 98

--- crates/flock-prover/src/bin/veiled_flock/tests.rs
+++ crates/flock-prover/src/bin/veiled_flock/tests.rs
@@ -1,7 +1,4 @@
-use super::{
-    MAX_BUNDLE_BYTES, decode_bundle, encode_digest_list, parse_digest_list,
-    validate_expected_digests,
-};
+use super::{MAX_BUNDLE_BYTES, decode_bundle};
 use flock_prover::proof_io::MAGIC;
 
 #[test]
@@ -22,29 +19,4 @@ fn decoder_rejects_an_unbounded_digest_vector() {
     bytes.push(5); // VEIL-FLOCK BLAKE3-preimage flavor.
     bytes.extend_from_slice(&u64::MAX.to_le_bytes());
     assert!(decode_bundle(&bytes).is_err());
-}
-
-#[test]
-fn digest_list_roundtrips_hex_lines() {
-    let digests = vec![[0x0a; 32], [0xf0; 32]];
-    let encoded = encode_digest_list(&digests);
-    assert_eq!(parse_digest_list(&encoded).unwrap(), digests);
-}
-
-#[test]
-fn digest_list_rejects_non_hex_or_wrong_width() {
-    assert!(parse_digest_list("abcd").is_err());
-    assert!(
-        parse_digest_list("00000000000000000000000000000000000000000000000000000000000000xz")
-            .is_err()
-    );
-}
-
-#[test]
-fn expected_digests_must_match_bundle_copy() {
-    let bundle_digests = vec![[1u8; 32]];
-    assert!(validate_expected_digests(&bundle_digests, &bundle_digests).is_ok());
-
-    let expected = vec![[2u8; 32]];
-    assert!(validate_expected_digests(&bundle_digests, &expected).is_err());
 }
